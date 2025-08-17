e9cb2dfb0c94d3305749dd65c0b863f94d110e5d
Carlos <caugustoc100@hotmail.com>
2025-08-17 02:08:38 +0000

    WIP

--- app.rb
+++ app.rb
@@ -15,44 +15,9 @@ Thread.new do
     )
 end
 
-# Configurar Sinatra para aceitar conexões externas
 set :bind, '0.0.0.0'
 set :logging, false
 set :logger, nil
-# set :port, 4567
-# get '/redis_health' do
-#   p "redis health"
-#   begin
-#     # status = REDIS.ping == 'PONG' ? 'UP' : 'DOWN'
-#     # code = status == 'UP' ? 200 : 500
-#     # [code, { status: status, redis_url: ENV['REDIS_URL'] }.to_json]
-#   rescue => e
-#     [500, { status: 'DOWN', error: e.message, redis_url: ENV['REDIS_URL'] }.to_json]
-#   end
-# end
-
-# get '/' do
-#   {foo: 'bar', app_instance: ENV['APP_INSTANCE']}.to_json
-# end
-
-# get '/bar' do
-#   # create a dataset from the items table
-#   items = DB[:items]
-
-#   # Use different random strings for the name field
-#   require 'securerandom'
-#   if items.count < 20
-#     3.times do
-#       items.insert(name: SecureRandom.hex(10), price: rand * 100)
-#     end
-#   end
-
-#   {
-#     app_instance: ENV['APP_INSTANCE'],
-#     items: items.all.to_json
-#   }.to_json
-# end
-
 
 get '/health' do
   {
@@ -60,24 +25,3 @@ get '/health' do
     app_instance: ENV['APP_INSTANCE']
   }.to_json
 end
-
-
-# get '/tables' do
-#   {
-#     tables: DB.tables
-#   }.to_json
-# end
-
-# get '/tables/:table_name' do
-#   table_name = params[:table_name].to_sym
-#   if DB.tables.include?(table_name)
-#     {
-#       columns: DB[table_name].columns
-#     }.to_json
-#   else
-#     status 404
-#     {
-#       error: "Table not found"
-#     }.to_json
-#   end
-# end
--- app/infra/workers.rb
+++ app/infra/workers.rb
@@ -1,24 +1,16 @@
 class Workers
   PAYMENTS_QUEUE = 'payments_queue'
   def self.configure_workers(queue_name: PAYMENTS_QUEUE, worker_count: 5, callback:)
-
-  #  p "Configure Workers"
     workers = []
 
-    # p "create the workers for #{queue_name}"
     worker_count.times do |i|
       workers << Thread.new do
         loop do
           begin 
-          # p "worker #{i}"
-          # p "retrieving data from queue #{queue_name}"
-          # _queue_name, payment_data = redis.brpop(queue_name, 2) # timeout de 2 segundos
+
           _queue_name, payment_data = Queue.pop(queue_name, 2) # timeout de 2 segundos
-          # p "get data"
-          # p payment_data
-          # p "process the payment"
+   
           callback.call(payment_data)
-          # process_payment_data(payment_data)
           rescue => e 
               p "Erro no worker #{i}: #{e.message}"
                p e.backtrace
